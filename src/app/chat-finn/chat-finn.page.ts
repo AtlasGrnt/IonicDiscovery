26af40002865011bcd053015117ed6d7fc65431e
Grégory Grenot <gregory.grenot@gmail.com>
2020-01-22 10:58:34 +0100

    FEAT: adding message fonction for Finn user

--- src/app/chat-finn/chat-finn.page.ts
+++ src/app/chat-finn/chat-finn.page.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-chat-finn',
   templateUrl: './chat-finn.page.html',
   styleUrls: ['./chat-finn.page.scss'],
 })
-export class ChatFinnPage implements OnInit {
+export class ChatFinnPage {
 
-  constructor() { }
+  messages = [
+    {
+      user: 'greg',
+      createdAt: 1554090856000,
+      msg: 'Yo ! Quoi de neuf ?'
+    },
+    {
+      user: 'finn',
+      createdAt: 1554090956000,
+      msg: 'yo, rien je chill avec rey et toi ?'
+    },
+    {
+      user: 'greg',
+      createdAt: 1554091056000,
+      msg: 'ah ah zero tu la choppe !'
+    }
+  ];
 
-  ngOnInit() {
+  currentUser = 'greg';
+  newMsg = '';
+  @ViewChild(IonContent) content: IonContent
+
+
+  constructor () { }
+
+  sendMessage(){
+    this.messages.push({
+      user: 'greg',
+      createdAt: new Date().getTime(),
+      msg: this.newMsg
+    });
+    this.newMsg = '';
+    setTimeout(() => {
+      this.content.scrollToBottom(200);
+    });
   }
-
 }
